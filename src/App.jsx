95a32bf3ce778ee40e8106e19d8646e67fccee24
Yash Parwani <parwaniyash5610@gmail.com>
2023-08-05 09:55:45 +0530

    redux

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,33 @@ import { Login, Register } from "./components"
 import db, { auth } from "./firebase";
 import { onAuthStateChanged } from "firebase/auth";
 import { collection, getDocs } from "firebase/firestore";
-
+import { useDispatch, useSelector } from "react-redux";
+import { login, logout, selectUser, setSubscription } from "./features/userSlice";
 const App = () => {
+  //fetching the user stored in the redux using useSelector
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
   
   useEffect(()=>{
     const unsubscribe = onAuthStateChanged(auth,userAuth =>{
       if(userAuth){
         // user is logged in
+        dispatch(login({
+          uid : userAuth.uid,
+          email : userAuth.email
+        }));
 
       }
       else{
         //user is logged out
+        dispatch(
+          logout()
+        );
       }
     })
 
     return unsubscribe;
-  },[])
+  },[dispatch])
 
   return (
      <div className="bg-blue-700">
